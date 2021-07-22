05452392941195e2f419793b45253551509048b1
Alexander Marsalek <amarsalek@iaik.tugraz.at>
2021-07-22 16:39:01 +0200

    more test files

--- pdf-as-pdfbox-2/src/test/java/at/gv/egiz/pdfas/lib/testpdfbox/SignatureFieldsAndPlaceHolderExtractorTest.java
+++ pdf-as-pdfbox-2/src/test/java/at/gv/egiz/pdfas/lib/testpdfbox/SignatureFieldsAndPlaceHolderExtractorTest.java
@@ -5,17 +5,30 @@ import at.gv.egiz.pdfas.lib.impl.placeholder.SignaturePlaceholderData;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.File;
+import java.util.List;
 
 public class SignatureFieldsAndPlaceHolderExtractorTest {
 
   public static void main(String[] args) {
     String dir = "/Users/amarsalek/Documents/pdf-as-4/pdf-as-pdfbox-2/src/test/resources/";
 
+    getPlacHolders(dir+ "manySignFields.pdf");
+    getPlacHolders(dir+ "manySignFields_signed4.pdf");
     getNextSignaturePlaceHolder(dir + "new_qr_2-2.pdf");
     getNextSignaturePlaceHolder(dir + "new_qr_2_signed.pdf");
     getNextSignaturePlaceHolder(dir + "new_qr_2_signed_signed.pdf");
     getNextSignaturePlaceHolder(dir + "new_qr_2_signed_signed_signed.pdf");
     //TODO convert into junit test
+  }
+
+  private static void getPlacHolders(String filePath) {
+    try {
+      PDDocument doc = PDDocument.load(new File(filePath));
+      List<String> result = SignatureFieldsAndPlaceHolderExtractor.findEmptySignatureFields(doc);
+      System.out.println(filePath + ": " + result);
+    } catch (Throwable e) {
+      e.printStackTrace();
+    }
   }
 
   public static void getNextSignaturePlaceHolder(String filePath) {

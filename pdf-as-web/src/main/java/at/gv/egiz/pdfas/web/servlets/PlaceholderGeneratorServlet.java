7617dbe08583cc8b7fa63670a7dbb860d3f25023
Alexander Marsalek <amarsalek@iaik.tugraz.at>
2021-07-22 16:39:23 +0200

    fix for issue #49

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/servlets/PlaceholderGeneratorServlet.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/servlets/PlaceholderGeneratorServlet.java
@@ -184,7 +184,7 @@ public class PlaceholderGeneratorServlet extends HttpServlet implements Placehol
 				g.drawString("placeholder for the", qrSize + ( 3 * border), start + lineSpace);
 				g.drawString("electronic signature", qrSize + ( 3 * border), start + (2 * lineSpace));
 			} else {
-				g.drawString("Platzhalter fuer die", qrSize + ( 3 * border), start + lineSpace);
+				g.drawString("Platzhalter für die", qrSize + ( 3 * border), start + lineSpace);
 				g.drawString("elektronische Signatur", qrSize + ( 3 * border), start + (2 * lineSpace));
 			}
 			if(id != null && !id.isEmpty()) {

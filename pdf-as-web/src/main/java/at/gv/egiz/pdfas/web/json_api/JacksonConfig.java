ab816bf0ab9b5a449dbb44a7c1d83890bd7c2c60
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-04-30 17:12:15 +0200

    fix json api to use names matching the soap api

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/json_api/JacksonConfig.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/json_api/JacksonConfig.java
@@ -2,6 +2,7 @@ package at.gv.egiz.pdfas.web.json_api;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.jakarta.xmlbind.JakartaXmlBindAnnotationModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,13 @@ public class JacksonConfig {
     return b -> b.featuresToEnable(
         SerializationFeature.WRITE_ENUMS_USING_TO_STRING,
         DeserializationFeature.READ_ENUMS_USING_TO_STRING
+    );
+  }
+
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer useJaxbJsonNames() {
+    return b -> b.modulesToInstall(
+        new JakartaXmlBindAnnotationModule()
     );
   }
 }

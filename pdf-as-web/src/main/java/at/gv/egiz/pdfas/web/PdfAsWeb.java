34d727e22d63b31252a60c6a2e36e0004ae72e64
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-05-13 17:44:17 +0200

    fix tomcat coop
    - properly initialize spring boot not just for bootRun but also for tomcat
    - fail-fast if properties fails to load
    - also deps bumps

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/PdfAsWeb.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/PdfAsWeb.java
@@ -2,9 +2,16 @@ package at.gv.egiz.pdfas.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PdfAsWeb {
+public class PdfAsWeb extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(PdfAsWeb.class);
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(PdfAsWeb.class, args);
   }

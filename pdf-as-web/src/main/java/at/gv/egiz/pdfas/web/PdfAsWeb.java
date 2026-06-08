e3c5b9a5229de2bf832301e3d025dd22a81aa49c
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-06-08 18:05:55 +0200

    map the legacy logback.configurationFile property to spring's logging.config

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/PdfAsWeb.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/PdfAsWeb.java
@@ -1,5 +1,6 @@
 package at.gv.egiz.pdfas.web;
 
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,6 +8,19 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class PdfAsWeb extends SpringBootServletInitializer {
+
+  static {
+    // compatibility layer: map the old logback.configurationFile to the new logging.config
+    final String SPRING_LOG_CFG = "logging.config";
+    final String LEGACY_LOGBACK_LOG_CFG = "logback.configurationFile";
+    if (System.getProperty(SPRING_LOG_CFG) == null) {
+      val legacyValue = System.getProperty(LEGACY_LOGBACK_LOG_CFG);
+      if (legacyValue != null) {
+        System.setProperty(SPRING_LOG_CFG, legacyValue);
+      }
+    }
+  }
+
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
     return application.sources(PdfAsWeb.class);

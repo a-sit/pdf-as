9ba8ba7c4dfd91515c32a891383ca47f1fe568bf
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-06-09 10:59:16 +0200

    log SOAP faults that are not caught by the default exception handler

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/servlets/SoapServiceServlet.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/servlets/SoapServiceServlet.java
@@ -3,8 +3,12 @@ package at.gv.egiz.pdfas.web.servlets;
 import jakarta.servlet.ServletConfig;
 import jakarta.xml.ws.Endpoint;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
+import org.apache.cxf.logging.FaultListener;
+import org.apache.cxf.message.Message;
 import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
 
 import at.gv.egiz.pdfas.web.ws.PDFASSigningImpl;
@@ -17,13 +21,32 @@ public class SoapServiceServlet extends CXFNonSpringServlet {
 	 */
 	private static final long serialVersionUID = -8903883276191902043L;
 
+	@Slf4j
+	public static class Faults implements FaultListener {
+		@Override
+		public boolean faultOccurred(Exception exception, String description, Message message) {
+			String operation = "-";
+			if (message != null && message.getExchange() != null) {
+				val boi = message.getExchange().getBindingOperationInfo();
+				if (boi != null && boi.getName() != null) {
+					operation = boi.getName().toString();
+				}
+			}
+
+			log.error("Unhandled SOAP fault in operation {}: {}", operation, description, exception);
+			return false;
+		}
+	}
+
 	@Override
 	protected void loadBus(ServletConfig sc) {
 		super.loadBus(sc);
 
 		// You could add the endpoint publish codes here
         Bus bus = BusFactory.newInstance(BusFactory.DEFAULT_BUS_FACTORY).createBus();
+		bus.setProperty(FaultListener.class.getName(), new Faults());
         BusFactory.setDefaultBus(bus);
+
         Endpoint signEp = Endpoint.publish("/wssign", new PDFASSigningImpl());
         /*
          * SOAPBinding signBinding = (SOAPBinding)signEp.getBinding();

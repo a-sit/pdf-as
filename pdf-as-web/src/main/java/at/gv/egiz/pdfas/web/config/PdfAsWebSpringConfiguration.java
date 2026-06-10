670d353a687636008b30ad1ef59a749366d12107
Jakob Heher <jakob.heher@iaik.tugraz.at>
2026-06-10 14:42:43 +0200

    use pdf-as-web.properties as a spring configuration source

--- pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/config/PdfAsWebSpringConfiguration.java
+++ pdf-as-web/src/main/java/at/gv/egiz/pdfas/web/config/PdfAsWebSpringConfiguration.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class PdfAsWebSpringConfiguration {
+  /** path to pdf-as-web.properties as loaded from spring sources */
   @Value("${pdf-as-web.conf}")
   @Getter
   @NonNull String pdfAsWebConfPath;
+
+  /** then also add pdf-as-web.properties itself as a spring property source (so it supplements application.properties) */
+  @Configuration
+  @PropertySource("file:${pdf-as-web.conf}")
+  public static class AsSpringPropertySource {}
 }
